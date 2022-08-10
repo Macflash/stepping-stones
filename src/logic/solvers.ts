6a3c7e8a740ec576cc406b721bc91cb8fe5362ca
Macflash <the.alex.breyfogle@gmail.com>
2022-08-09 21:48:33 -0700

    solver todo

--- src/logic/solvers.ts
+++ src/logic/solvers.ts
@@ -1,7 +1,15 @@
+import { GridCount } from "./grid";
 import { Grid } from "./types";
 
 export function BruteForceGridSolver(grid: Grid): Grid {
   let bestGrid = grid;
 
+  // TODO: Solve NP-hard problem, prove P=NP
+
   return bestGrid;
+}
+
+export function FindNextOne(grid: Grid) {
+  const counts = GridCount(grid);
+  const max = counts.length;
 }
